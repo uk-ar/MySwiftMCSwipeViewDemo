1168d03aa39c3772b901437b768cd551a483e2cd
uk-ar <yuuki.ari@gmail.com>
2015-02-28 06:45:16 +0900

    Add TableViewCell

---
+++ MySwiftMCSwipeViewDemo/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  MySwiftMCSwipeViewDemo
+//
+//  Created by 有澤 悠紀 on 2015/02/28.
+//  Copyright (c) 2015年 有澤 悠紀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ MySwiftMCSwipeViewDemo/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  MySwiftMCSwipeViewDemo
+//
+//  Created by 有澤 悠紀 on 2015/02/28.
+//  Copyright (c) 2015年 有澤 悠紀. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
