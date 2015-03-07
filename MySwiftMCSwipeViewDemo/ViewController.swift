3e88fd9d25795af10b3fb9361f978e6e24093c97
uk-ar <yuuki.ari@gmail.com>
2015-03-08 07:18:40 +0900

    ADD UISegmentedControl only

--- MySwiftMCSwipeViewDemo/ViewController.swift
+++ MySwiftMCSwipeViewDemo/ViewController.swift
@@ -14,10 +14,69 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var unreadItems = ["TEST1", "TEST2", "TEST3"]
     var doneItems = []
     var tableView:UITableView?
-    
+    let mySegLabel: UILabel = UILabel(frame: CGRectMake(0,0,150,150))
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // setTableView()
+        
+        // 表示する配列を作成する.
+        let myArray: NSArray = ["Red","Blue","Green"]
+        
+        // SegmentedControlを作成する.
+        let mySegcon: UISegmentedControl = UISegmentedControl(items: myArray)
+        mySegcon.center = CGPoint(x: self.view.frame.width/2, y: 400)
+        mySegcon.backgroundColor = UIColor.grayColor()
+        mySegcon.tintColor = UIColor.whiteColor()
+        
+        // イベントを追加する.
+        mySegcon.addTarget(self, action: "segconChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        
+        // Viewに追加する.
+        self.view.addSubview(mySegcon)
+        
+        // Labelを作成する.
+        mySegLabel.backgroundColor = UIColor.whiteColor()
+        mySegLabel.layer.masksToBounds = true
+        mySegLabel.layer.cornerRadius = 75.0
+        mySegLabel.textColor = UIColor.whiteColor()
+        mySegLabel.shadowColor = UIColor.grayColor()
+        mySegLabel.font = UIFont.systemFontOfSize(CGFloat(30))
+        mySegLabel.textAlignment = NSTextAlignment.Center
+        mySegLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
+        
+        // Viewの背景色をCyanにする.
+        self.view.backgroundColor = UIColor.cyanColor()
+        
+        // Viewに追加する.
+        self.view.addSubview(mySegLabel);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func segconChanged(segcon: UISegmentedControl){
+        
+        switch(segcon.selectedSegmentIndex){
+        case 0:
+            mySegLabel.backgroundColor = UIColor.redColor()
+            
+        case 1:
+            mySegLabel.backgroundColor = UIColor.blueColor()
+            
+        case 2:
+            mySegLabel.backgroundColor = UIColor.greenColor()
+            
+        default:
+            println("Error")
+            
+        }
+    }
+    
+    func setTableView(){
         // Status Barの高さを取得する.
         let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
         
@@ -39,11 +98,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         // Viewに追加する.
         self.view.addSubview(self.tableView!)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     func viewWithImageName(name: String) -> UIView {
